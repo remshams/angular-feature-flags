6bd7e6b7eef0bf10a0a3c79db894413a5a05ccfc
Mathias Remshardt <remshams@gmail.com>
2023-06-24 14:40:58 +0200

    feat(FeatureService): adds triggering loading of feature.json on app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FeatureService } from './feature-flags/feature.service';
+
+const initFeatureServiceFactory = (featureService: FeatureService) => {
+  return () => featureService.init();
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initFeatureServiceFactory,
+      deps: [FeatureService],
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
